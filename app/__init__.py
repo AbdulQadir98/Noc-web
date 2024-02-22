b5f1951897ed8bce8f75d203ce7876b9ef88bd74
AbdulQadir98 <abdul.qadirv98@gmail.com>
2024-02-22 22:25:01 +0530

    cv

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-import cv2
 from app.framework.framework import NocturneFramework
 from app.models import *
 from app.views import *
@@ -9,11 +8,6 @@ framework = NocturneFramework()
 
 # Create a Flask application instance
 app = Flask(__name__)
-
-
-@app.route('/hello')
-def hello():
-    return framework.say_hello("Vili")
 
 
 @app.route('/')
@@ -39,22 +33,6 @@ def upload_model():
     return redirect(url_for('home'))
 
 
-# capturing frames from the camera and yielding them as continuous stream of JPEG images.
-def camera_feed():
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
 @app.route('/video_feed')
 def video_feed():
-    return Response(camera_feed(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(framework.start_video(), mimetype='multipart/x-mixed-replace; boundary=frame')
--- app/framework/framework.py
+++ app/framework/framework.py
@@ -1,11 +1,48 @@
 import os
+import cv2
+from ultralytics import YOLO
+
 
 class NocturneFramework:
-    def __init__(self, greeting="Hello"):
-        self.greeting = greeting
+    def __init__(self, camera_index=0, app='web', model_path='yolov5s.pt'):
+        self.camera_index = camera_index
+        self.app = app
+        self.cap = cv2.VideoCapture(camera_index)
+        # self.model = YOLO(model_path)
+        self.running = False
 
-    def say_hello(self, name):
-        return f"{self.greeting}, {name}!"
+    def __del__(self):
+        self.cap.release()
+
+    def process_frame(self, frame):
+        results = self.model(frame)
+        for detection in results:
+            x, y, w, h, class_id, confidence = detection
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, f'{self.model.names[int(class_id)]} {confidence:.2f}', (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        return frame
+
+    def start_video(self):
+        self.running = True
+        while self.running:
+            success, frame = self.cap.read()
+            if not success:
+                break
+
+            # frame = self.process_frame(frame)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            if self.app == 'web':
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            else:
+                return frame
+
+    def stop_video(self):
+        self.running = False
 
     def get_models(self):
         folder_path = "app/models"
